af6ef825a2854998f25b3b1b1085f4d92dc163ff
Ricardo Pérez <ricpelo@gmail.com>
2021-04-22 19:31:40 +0200

    Jueves 22 de abril

--- java/Animales.java
+++ java/Animales.java
@@ -1,23 +1,23 @@
-class Animal {
-    public int getNumeroPatas() {
-        return numeroPatas;
-    }
+// class Animal {
+//     public int getNumeroPatas() {
+//         return numeroPatas;
+//     }
 
-    public int setNumeroPatas(int numeroPatas) {
-        this.numeroPatas = numeroPatas;
-        return getNumeroPatas();
-    }
+//     public int setNumeroPatas(int numeroPatas) {
+//         this.numeroPatas = numeroPatas;
+//         return getNumeroPatas();
+//     }
 
-    private int numeroPatas = 5;
-}
+//     private int numeroPatas = 5;
+// }
 
-public class Animales {
-    public static void main(String[] args) {
-        Animal a = new Animal();
+// public class Animales {
+//     public static void main(String[] args) {
+//         Animal a = new Animal();
 
-        System.out.println(pepe);
-        a.setNumeroPatas(9);
-        System.out.println(a.getNumeroPatas());
-    }
-    static int pepe = 5;
-}
+//         System.out.println(pepe);
+//         a.setNumeroPatas(9);
+//         System.out.println(a.getNumeroPatas());
+//     }
+//     static int pepe = 5;
+// }
--- java/BaseDerivada.java
+++ java/BaseDerivada.java
@@ -1,19 +1,52 @@
-class Base {
-    protected void imprimir() {
-        System.out.println("Hola");
+class Animal {
+    int numeroPatas;
+
+    public int getNumeroPatas() {
+        System.out.println(nombre());
+        return numeroPatas;
     }
 
+    public String nombre() {
+        return "Animal";
+    }
+
+    protected void mover(String w) {
+        System.out.println("Me estoy moviendo hacia el " + w);
+    }
 }
 
-class Derivada extends Base {   // Derivada <1 Base
-    public void imprimirDesdeDerivada() {
-        System.out.println("Hola desde Derivada");
+class Gato extends Animal {
+    String numeroPatas;
+
+    @Override
+    public String nombre() {
+        return "Gato";
+    }
+
+    @Override
+    protected void mover(String s) {
+        System.out.println("Me muevo como un gato hacia el " + s);
+    }
+
+    public void maullar() {
+        System.out.println("Miauuuuuuuu");
+    }
+}
+
+class Perro extends Animal {
+    public void ladrar() {
+        System.out.println("Guauuuuuuu");
+    }
+
+    @Override
+    protected void mover(String s) {
+        System.out.println("Me voy saltando y brincando hacia el " + s);
     }
 }
 
 public class BaseDerivada {
     public static void main(String[] args) {
-        Base b = new Derivada();
-        ((Derivada) b).imprimirDesdeDerivada();
+        Gato a = new Gato();
+        System.out.println(a.getNumeroPatas());
     }
 }
